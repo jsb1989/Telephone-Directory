4aeee1d9549c1839eae32df9ceacd3afe39d8fc5
Joshua Barnett <51182988+jsb1989@users.noreply.github.com>
2020-08-24 21:15:03 -0400

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,16 @@
+public class Node{
+	Node next;
+	Node prev;
+	String FirstName; 
+    	String LastName;
+    	String PhoneNumber;
+    	String Email;
+    	String WorkSchool;
+    	public Node(String F,String L,String P,String E,String W){ 
+          this.FirstName=F; 
+          this.LastName=L;
+          this.PhoneNumber=P;
+          this.Email=E;
+          this.WorkSchool=W;
+        } 
+}
