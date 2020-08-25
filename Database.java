62e7529a59588228770c1e7d323782704553d177
Joshua Barnett <51182988+jsb1989@users.noreply.github.com>
2020-08-24 21:14:34 -0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,21 +1,3 @@
 public class Database{
-  Node head;
-  //Contents of a Doubly Linked List Node
-  class Node { 
-    String FirstName; 
-    String LastName;
-    String PhoneNumber;
-    String Email;
-    String WorkSchool;
-    Node prev;
-    Node next;
-        //Constructor of a Doubly Linked List Node
-        Node(String F,String L,String P,String E,String W){ 
-          this.FirstName=F; 
-          this.LastName=L;
-          this.PhoneNumber=P;
-          this.Email=E;
-          this.WorkSchool=W;
-        } 
-    } 
+	Node head;
 }
